df71f3b2ccd44cbc358aca19daa8f1efb157423f
Lennard Claproth <0956550@hr.nl>
2018-03-27 12:59:30 +0200

    Added Test class to test Git

---
+++ ZonderZorgenGit/Controller/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZonderZorgenGit.Controller
+{
+    class Class1
+    {
+    }
+}
