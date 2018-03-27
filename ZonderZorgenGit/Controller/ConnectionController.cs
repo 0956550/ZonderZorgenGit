56675e31e94741d5462d18b5c2a73d6754a55835
Lennard Claproth <0956550@hr.nl>
2018-03-27 13:21:24 +0200

    Forgot to save chagens

--- ZonderZorgenGit/Controller/ConnectionController.cs
+++ ZonderZorgenGit/Controller/ConnectionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,64 @@ using System.Threading.Tasks;
 
 namespace ZonderZorgenGit.Controller
 {
-    class Class1
+    class ConnectionController
     {
+        private MySqlConnection connection;
+        private bool connectionStatus = false;
+
+        public ConnectionController()
+        {
+        }
+
+        public void Initialize(String server, String port, String database, string userId, string password)
+        {
+            string connectionString;
+            connectionString = string.Format("Server=" + server + ";" + "Port=" + port + ";" + "Database=" + database + ";" + "Uid=" + userId + ";" + "Pwd=" + password + ";");
+            connection = new MySqlConnection(connectionString);
+
+            if (OpenConnection())
+            {
+                connectionStatus = true;
+            }
+            else
+            {
+                connectionStatus = false;
+            }
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                return false;
+            }
+        }
+
+        public bool GetConnectionStatus()
+        {
+            return connectionStatus;
+        }
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
     }
 }
--- ZonderZorgenGit/LoginView.Designer.cs
+++ ZonderZorgenGit/LoginView.Designer.cs
@@ -28,12 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginView));
+            this.LogoImg = new System.Windows.Forms.PictureBox();
+            this.ConnectionStatusLbl = new System.Windows.Forms.Label();
+            this.ServerIpLbl = new System.Windows.Forms.Label();
+            this.DatabaseLbl = new System.Windows.Forms.Label();
+            this.PasswordLbl = new System.Windows.Forms.Label();
+            this.UserIdLbl = new System.Windows.Forms.Label();
+            this.PortLbl = new System.Windows.Forms.Label();
+            this.ServerIpTxtBx = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LogoImg
+            // 
+            this.LogoImg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LogoImg.Cursor = System.Windows.Forms.Cursors.No;
+            this.LogoImg.Image = ((System.Drawing.Image)(resources.GetObject("LogoImg.Image")));
+            this.LogoImg.Location = new System.Drawing.Point(221, 12);
+            this.LogoImg.Name = "LogoImg";
+            this.LogoImg.Size = new System.Drawing.Size(250, 100);
+            this.LogoImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoImg.TabIndex = 0;
+            this.LogoImg.TabStop = false;
+            // 
+            // ConnectionStatusLbl
+            // 
+            this.ConnectionStatusLbl.AutoSize = true;
+            this.ConnectionStatusLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ConnectionStatusLbl.Location = new System.Drawing.Point(12, 432);
+            this.ConnectionStatusLbl.Name = "ConnectionStatusLbl";
+            this.ConnectionStatusLbl.Size = new System.Drawing.Size(60, 20);
+            this.ConnectionStatusLbl.TabIndex = 1;
+            this.ConnectionStatusLbl.Text = "Status:";
+            // 
+            // ServerIpLbl
+            // 
+            this.ServerIpLbl.AutoSize = true;
+            this.ServerIpLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ServerIpLbl.Location = new System.Drawing.Point(12, 139);
+            this.ServerIpLbl.Name = "ServerIpLbl";
+            this.ServerIpLbl.Size = new System.Drawing.Size(78, 20);
+            this.ServerIpLbl.TabIndex = 2;
+            this.ServerIpLbl.Text = "Server IP:";
+            // 
+            // DatabaseLbl
+            // 
+            this.DatabaseLbl.AutoSize = true;
+            this.DatabaseLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.DatabaseLbl.Location = new System.Drawing.Point(12, 171);
+            this.DatabaseLbl.Name = "DatabaseLbl";
+            this.DatabaseLbl.Size = new System.Drawing.Size(85, 20);
+            this.DatabaseLbl.TabIndex = 3;
+            this.DatabaseLbl.Text = "DataBase:";
+            // 
+            // PasswordLbl
+            // 
+            this.PasswordLbl.AutoSize = true;
+            this.PasswordLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.PasswordLbl.Location = new System.Drawing.Point(12, 235);
+            this.PasswordLbl.Name = "PasswordLbl";
+            this.PasswordLbl.Size = new System.Drawing.Size(82, 20);
+            this.PasswordLbl.TabIndex = 4;
+            this.PasswordLbl.Text = "Password:";
+            // 
+            // UserIdLbl
+            // 
+            this.UserIdLbl.AutoSize = true;
+            this.UserIdLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.UserIdLbl.Location = new System.Drawing.Point(12, 203);
+            this.UserIdLbl.Name = "UserIdLbl";
+            this.UserIdLbl.Size = new System.Drawing.Size(68, 20);
+            this.UserIdLbl.TabIndex = 5;
+            this.UserIdLbl.Text = "User ID:";
+            // 
+            // PortLbl
+            // 
+            this.PortLbl.AutoSize = true;
+            this.PortLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.PortLbl.Location = new System.Drawing.Point(12, 267);
+            this.PortLbl.Name = "PortLbl";
+            this.PortLbl.Size = new System.Drawing.Size(42, 20);
+            this.PortLbl.TabIndex = 6;
+            this.PortLbl.Text = "Port:";
+            // 
+            // ServerIpTxtBx
+            // 
+            this.ServerIpTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ServerIpTxtBx.Location = new System.Drawing.Point(122, 136);
+            this.ServerIpTxtBx.Name = "ServerIpTxtBx";
+            this.ServerIpTxtBx.Size = new System.Drawing.Size(149, 26);
+            this.ServerIpTxtBx.TabIndex = 7;
+            this.ServerIpTxtBx.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(122, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 26);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(122, 200);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox4.Location = new System.Drawing.Point(122, 232);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 26);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox5.Location = new System.Drawing.Point(122, 264);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(149, 26);
+            this.textBox5.TabIndex = 11;
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ServerIpTxtBx);
+            this.Controls.Add(this.PortLbl);
+            this.Controls.Add(this.UserIdLbl);
+            this.Controls.Add(this.PasswordLbl);
+            this.Controls.Add(this.DatabaseLbl);
+            this.Controls.Add(this.ServerIpLbl);
+            this.Controls.Add(this.ConnectionStatusLbl);
+            this.Controls.Add(this.LogoImg);
+            this.Name = "LoginView";
+            this.Text = "ZonderZorgen";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox LogoImg;
+        private System.Windows.Forms.Label ConnectionStatusLbl;
+        private System.Windows.Forms.Label ServerIpLbl;
+        private System.Windows.Forms.Label DatabaseLbl;
+        private System.Windows.Forms.Label PasswordLbl;
+        private System.Windows.Forms.Label UserIdLbl;
+        private System.Windows.Forms.Label PortLbl;
+        private System.Windows.Forms.TextBox ServerIpTxtBx;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
