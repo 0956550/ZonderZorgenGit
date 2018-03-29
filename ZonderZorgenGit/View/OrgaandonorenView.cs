3cf25a2de7fd8b655b3c03e1d7d64ba065112c94
Lennard Claproth <0956550@hr.nl>
2018-03-29 13:37:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- ZonderZorgenGit/View/OrgaandonorenView.cs
+++ ZonderZorgenGit/View/OrgaandonorenView.cs
@@ -26,7 +26,6 @@ namespace ZonderZorgenGit.View
         private void DashboardBtn_Click(object sender, EventArgs e)
         {
             DashboardController dashboardController = new DashboardController(this.mainConnection);
-            Console.WriteLine("Test 3");
             this.Close();
         }
 
